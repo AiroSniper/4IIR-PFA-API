ca4997a82d4852b4293bafb53e04397c493bf175
Smyejfatimaezzahra <fatimaezzahra.smyej@edu.uca.ma>
2022-05-19 22:14:14 +0000

    smyej

--- routes/file.js
+++ routes/file.js
@@ -5,6 +5,26 @@ const fileUpload=require("express-fileupload");
 router.use(fileUpload());
 
 
+var fs = require("fs");
+
+router.post('/', async (req,res)=>{
+    const file = new File({
+        file_url: req.body.file_url,
+        width: req.body.width,
+        height: req.body.height,
+        date_captured: req.body.date_captured,
+        gps_location:req.body.gps_location,
+        objects:req.body.objects 
+       });
+       try{
+        const savedFile = await file.save();
+        res.json(savedFile);
+       }
+       catch(err){
+        res.json({message:err});
+       }
+    
+});
 
 router.get('/',async (req,res)=>{
     try{
